b27957e87a69c7b3ba18bcd225347321f450d26d
javi <none@none>
2010-12-22 19:34:23 +0100

    dispositivo y punto de montaje por linea de comandos

--- clauerfs.c
+++ clauerfs.c
@@ -1,4 +1,5 @@
 #define FUSE_USE_VERSION  26
+#define VERSION "0.0.1"
 
 #define DEVICE "/dev/sdc4"
 #define MAX_DEVICE_LEN 20
@@ -13,6 +14,7 @@
 #define BYTES_TIPO			1
    
 #include <fuse.h>
+#include <fuse_opt.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
@@ -48,6 +50,37 @@ typedef struct file_map file_map_t;
 
 file_map_t *fm = NULL;
 char * pwd = "h0l4c4r4c0l4";
+char * mountpoint;
+char * device;
+
+/** options for fuse_opt.h */
+struct options {
+   char* password;
+}options;
+
+/** macro to define options */
+#define CLAUER_OPT_KEY(t, p, v) { t, offsetof(struct options, p), v }
+
+/** keys for FUSE_OPT_ options */
+enum {
+   KEY_VERSION,
+   KEY_HELP,
+   KEY_DEBUG,
+};
+
+static struct fuse_opt clauer_opts[] = {
+		CLAUER_OPT_KEY("-p %s", password, 0),
+		CLAUER_OPT_KEY("--password=%s", password, 0),
+
+		FUSE_OPT_KEY("-V",             KEY_VERSION),
+		FUSE_OPT_KEY("--version",      KEY_VERSION),
+		FUSE_OPT_KEY("-h",             KEY_HELP),
+		FUSE_OPT_KEY("--help",         KEY_HELP),
+		FUSE_OPT_KEY("-d",				KEY_DEBUG),
+		FUSE_OPT_KEY("--debug",		KEY_DEBUG),
+		FUSE_OPT_END
+};
+
 
 int rellenar_hash(){
 	int i, j, tam;
@@ -241,13 +274,76 @@ static struct fuse_operations clauer_oper = {
     .read   = clauer_read,
 };
 
+static void usage( const char *progname ) {
+	fprintf(stderr,
+			"usage: %s devicepath mountpoint [options]\n"
+			"\n"
+			"general options:\n"
+			"    -o opt,[opt...]        mount options\n"
+			"    -h   --help            print help\n"
+			"    -V   --version         print version\n"
+			"\n",progname);
+}
+
+static int clauer_opt_proc(void *data, const char *arg, int key, struct fuse_args *outargs) {
+        (void) data;
+
+        switch( key ) {
+			case FUSE_OPT_KEY_OPT:
+				return 1;
+
+			case FUSE_OPT_KEY_NONOPT:
+				if( !device ) {
+						device = strdup(arg);
+						return 0;
+				} else if( !mountpoint ) {
+						mountpoint = strdup(arg);
+				}
+				return 1;
+
+			case KEY_HELP:
+					usage(outargs->argv[0]);
+					fuse_opt_add_arg(outargs, "-ho");
+					fuse_main( outargs->argc, outargs->argv, &clauer_oper, NULL );
+					exit(1);
+
+			case KEY_VERSION:
+					fprintf( stderr, "clauerfs version %s\n", VERSION );
+					fuse_opt_add_arg(outargs, "--version");
+					fuse_main( outargs->argc, outargs->argv, &clauer_oper, NULL );
+					exit(0);
+			case KEY_DEBUG:
+					fuse_opt_add_arg(outargs, "-d");
+			default:
+					fprintf(stderr, "internal error\n");
+					abort();
+        }
+}
+
 int main(int argc, char *argv[]){
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 	int res;
 	file_map_t *current_fm;
 	
-	handle.hDevice = open(DEVICE,O_RDONLY);
+	/* clear structure that holds our options */
+	memset(&options, 0, sizeof(struct options));
+	if (fuse_opt_parse(&args, &options, clauer_opts, clauer_opt_proc) == -1)
+	         /** error parsing options */
+	         return -1;
+	if( device==NULL ) {
+		fprintf(stderr, "missing device path\n");
+	    fprintf(stderr, "see `%s -h' for usage\n", argv[0]);
+	    exit(1);
+	}
+	if( mountpoint==NULL ) {
+		fprintf(stderr, "missing mount point\n");
+	    fprintf(stderr, "see `%s -h' for usage\n", argv[0]);
+	    exit(1);
+	}
+
+	handle.hDevice = open(device,O_RDONLY);
 	lseek(handle.hDevice, 0, SEEK_SET);
-	strcpy(handle.device,DEVICE);
+	strcpy(handle.device,device);
 	
 	if (read(handle.hDevice, (void *) &(handle.ib), BLOCK_SIZE) == -1)
 		return -errno;
@@ -257,7 +353,7 @@ int main(int argc, char *argv[]){
 	CRYPTO_Ini();
 	if ((res = rellenar_hash()) != 0)
 		return res;
-    res = fuse_main(argc, argv, &clauer_oper, NULL);
+    res = fuse_main(args.argc, args.argv, &clauer_oper, NULL);
     CRYPTO_Fin();
     close(handle.hDevice);
     while (fm){
